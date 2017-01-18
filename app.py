737ab1cd58add48fc20225423ee8eee7d56bbd74
brandonxiang <1542453460@qq.com>
2017-01-18 10:49:53 +0800

    test

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ def vote():
     conn = g.db.cursor()
     cmd = "insert into validation (openid) values ('%s')"
     conn.execute(cmd, (openid))
-    conn.commit()
 
     # cmd = "update vote set score= %s where name = %s"
     # conn.executemany(cmd, score)
